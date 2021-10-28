ea335e11c821d82c8d96869285a1a84c2f89db09
artem <vrail30@outlook.com>
2021-10-28 16:54:46 +0300

    Task done.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 from typing import Type
+import functools
 
 
+@functools.total_ordering
 class Currency:
     """
     1 EUR = 2 USD = 100 RUB
@@ -11,24 +13,66 @@ class Currency:
     1 RUB = 0.02 USD ;  1 RUB = 0.01 EUR
     """
 
-    def __init__(self, value: float):
-        pass
+    def __init__(self, value: float = 0):
+        self.value = float(value)
 
     @classmethod
-    def course(cls, other_cls: Type[Currency]) -> Currency:
-        raise NotImplementedError
+    def course(cls, other_cls: Type[Currency]) -> str:
+        return f"{cls.__dict__['rates'][other_cls()]} {other_cls()} for 1 {cls()}"
 
-    def to_currency(self, other_cls: Type[Currency]):
-        raise NotImplementedError
+    def to_currency(self, other_cls: Type[Currency]) -> Currency:
+        return other_cls(
+            self.value * self.__class__.__dict__['rates'][str(other_cls())])
+
+    def __add__(self: Currency, other: Currency) -> Currency:
+        return other.__class__(
+            self.to_currency(other.__class__).value + other.value
+        )
+
+    def __gt__(self: Currency, other: Currency) -> bool:
+        return self.to_currency(other.__class__).value > other.value
+
+    def __le__(self, other):
+        return self.to_currency(other.__class__).value < other.value
+
+    def __eq__(self, other):
+        return self.to_currency(other.__class__).value == other.value
 
 
 class Euro(Currency):
-    pass
+    rates = {
+        'USD': 2,
+        'RUB': 100
+    }
+
+    def __repr__(self):
+        if self.value:
+            return f"{self.value} EUR"
+        else:
+            return "EUR"
 
 
 class Dollar(Currency):
-    pass
+    rates = {
+        'EUR': 0.5,
+        'RUB': 50
+    }
+
+    def __repr__(self):
+        if self.value:
+            return f"{self.value} USD"
+        else:
+            return "USD"
 
 
 class Rubble(Currency):
-    pass
+    rates = {
+        'USD': 0.02,
+        'EUR': 0.01
+    }
+
+    def __repr__(self):
+        if self.value:
+            return f"{self.value } RUB"
+        else:
+            return "RUB"
